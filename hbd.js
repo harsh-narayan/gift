cf5d95725cdbe3ac1a38f0651554385236b2716b
harsh-narayan <69381567+harsh-narayan@users.noreply.github.com>
2021-08-25 15:47:02 +0530

    Update hbd.js

--- hbd.js
+++ hbd.js
@@ -1,6 +1,12 @@
 function sub() {
     var plan = document.getElementsByName('plan');
     var mail = document.getElementById("mail").value;
+    var p;
+    for(var i=0; i<plan.length; ++i){
+        if(plan[i].checked)
+            p = plan[i].value;
+    }
 
-    window.alert(plan + mail);
+
+    window.alert("thank you for subscribing to " + p + ". i wil communicate with you through " + mail + " plan. Send a screenshot of this to me on whatsapp to verify the purchase.");
 }
